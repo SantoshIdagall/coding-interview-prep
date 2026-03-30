0c244e663ae101bc07f2ddca55863d64155ca4bd
Santosh Idagall <santuidagall18@gmail.com>
2026-03-31 00:22:07 +0530

    First commit - added initial coding files

---
+++ Arraymax.java
@@ -0,0 +1,22 @@
+class Arraymax{
+
+    public static int findbyMax(int[] arr){
+        int max=arr[0];
+        for(int num:arr){
+            if(num>max){
+                max=num;
+            }
+        }
+      return max;
+
+    }
+    public static void main(String[]args){
+        int []arr ={1,2,3,4,5};
+        int max=findbyMax(arr);
+        System.out.println("The maximum element in the array is: "+max);
+
+        } 
+
+    }
+
+
---
+++ ReverseArray.java
@@ -0,0 +1,34 @@
+class ReverseArray{
+    // basic way to reverse an array
+   /*  public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        
+        for(int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+        */
+
+   // using two pointer approach to reverse an array
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+            // swap arr[left] and arr[right]
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+
+}

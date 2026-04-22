bfa70b72be4e9998261846d8f6d56d21109d9406
Santosh Idagall <santuidagall18@gmail.com>
2026-04-22 23:05:17 +0530

    add the code

---
+++ arrays/MissingNumber.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class MissingNumber {
+     public static int missingnumber(int [] arr , int n){
+        int  esum = n*(n+1)/2 ;
+        int asum=0;
+
+        for(int nums : arr){
+            asum += nums;
+        }
+
+        return esum-asum;
+
+     }
+    
+     public static void main(String[] args) {
+        int [] arr ={1,2,3,4,5,6,9,10,7};
+        System.out.println(missingnumber(arr, 10));
+     }
+}

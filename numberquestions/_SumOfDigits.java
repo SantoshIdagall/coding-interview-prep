8fd97647807af0b3e7546ce9b86be03170f13ec4
Santosh Idagall <santuidagall18@gmail.com>
2026-04-24 22:27:47 +0530

    Add SumOfDigits program

---
+++ numberquestions/_SumOfDigits.java
@@ -0,0 +1,20 @@
+package numberquestions;
+
+public class _SumOfDigits {
+    public static  int sumofdigits(int num){
+         num = Math.abs(num); // handle negative
+        int sum =0;
+         while (num>0) {
+           sum += num % 10; 
+           num /= 10; 
+            
+         }
+         return sum;
+    }
+
+    public static void main(String[] args) {
+        int num =123546;
+        System.out.println(sumofdigits(num));
+    }
+    
+}

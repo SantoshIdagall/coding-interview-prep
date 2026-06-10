0b63527698c7b60c754cc058ae58562f406b1529
Santosh Idagall <122441463+SantoshIdagall@users.noreply.github.com>
2026-06-10 22:26:11 +0530

    Update _PrimeNumber.java

--- numberquestions/_PrimeNumber.java
+++ numberquestions/_PrimeNumber.java
@@ -3,8 +3,7 @@ package numberquestions;
 public class _PrimeNumber {
     public static  boolean isPrime(int num){
         if (num <= 1)  return false;
-            for( int i = 2; i*i<=num; i++){
-
+            for( int i = 2;  i*i<=num; i++){
             if(num % i== 0)
                  return false; 
      

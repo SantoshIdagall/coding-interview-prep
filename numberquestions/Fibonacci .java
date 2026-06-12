88101f49d2f78bffd88c8d8ab077ddfa2f188e57
Santosh Idagall <122441463+SantoshIdagall@users.noreply.github.com>
2026-06-12 20:04:45 +0530

    Update Fibonacci .java

--- numberquestions/Fibonacci .java
+++ numberquestions/Fibonacci .java
@@ -1,9 +1,9 @@
 package numberquestions;
 
 public class Fibonacci  {
-    public static void fibonacci(int n) { 
+    public static void fibonacci(int num) { 
 int n1 = 0, n2 = 1; 
-for (int i = 0; i < n; i++) { 
+for (int i = 0; i < num; i++) { 
 System.out.print(n1+ " " + n2); 
 int n3 = n1 + n2; 
     System.out.println(n3);
@@ -13,8 +13,8 @@ int n3 = n1 + n2;
     }
 
 public static void main(String[] args) {
-    int n = 10 ;
-     System.out.println(fibonacci(n));
+    int num = 10 ;
+     System.out.println(fibonacci(num));
 }
     
 }
